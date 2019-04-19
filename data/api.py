2f226f95e67fc599af4cafd317bd09542fd4c31b
masaintmartin <matthieu.saint-martin@ynov.com>
2019-04-19 12:05:28 +0200

    Add day in year value

--- data/api.py
+++ data/api.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import datetime
 
 def getAllData():
       req_2019 = requests.get("https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=234400034_temperature-quotidienne-regionale-2016-2017&rows=365&sort=-date&facet=date&facet=region&refine.date=2019")
@@ -21,8 +22,25 @@ def processJson(json):
       for data in json:
             tmpJson = {}
             tmpJson["date"] = data["fields"]["date"]
+            tmpJson["dayInYear"] = dayInYearFromString(data["fields"]["date"])
             tmpJson["tmin"] = data["fields"]["tmin"]
             tmpJson["tmoy"] = data["fields"]["tmoy"]
             tmpJson["tmax"] = data["fields"]["tmax"]
             processedJson.append(tmpJson)
       return processedJson
+
+def dayInYearFromString(str):
+      monthDays = [31,28,31,30,31,30,31,31,30,31,30,31]
+      date = datetime.datetime.strptime(str, "%Y-%m-%d")
+
+      value = 0
+
+      if(date.date().day == 29 & date.date().month == 2):
+            value+=1
+
+      for x in range(date.date().month-1):
+            value += monthDays[x]
+            
+      value += date.date().day
+
+      return value
