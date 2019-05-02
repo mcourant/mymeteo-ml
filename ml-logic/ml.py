8a663a718caee221ffe98aa09c47944ec32fe7ad
masaintmartin <matthieu.saint-martin@ynov.com>
2019-05-02 15:19:16 +0200

    Try de l'echec

--- ml-logic/ml.py
+++ ml-logic/ml.py
@@ -1,10 +1,12 @@
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
+# Linear
+# Descartes
+
 def handleData(data):
     df = pd.DataFrame(data)
     
-    # x = df.drop(labels="tmin", axis=1)
     x = df.drop(labels=["tmax", "date", "tmoy"], axis=1)
     y = df["tmin"]
     y = y.astype('int')
@@ -23,8 +25,32 @@ def handleData(data):
 
     model.fit(X_train, y_train)
     
-    preds = model.predict(X_test)
+    predict = model.predict(X_test)
 
     model.predict_proba(X_test)
 
     print(model.score(x, y))
+
+
+    from sklearn.linear_model import LinearRegression
+
+    reg = LinearRegression()
+
+    reg.fit(x, y)
+
+    # reg.predict(X_test)
+
+    print(reg.score(x, y))
+
+    test_var = []
+    test_var.append([163,15])
+    test_var.append([164,15])
+    test_var.append([165,15])
+    test_var.append([166,15])
+    test_var.append([167,15])
+
+    p = reg.predict(test_var)
+
+    print(p)
+
+    # print(reg.score(test_var, test_res))
