1f23b21a82fec2f34005fc669a409d63ae86ccc9
Maxime COURANT <maxime.courant@ynov.com>
2019-05-03 14:45:11 +0200

    That work for two day = 166 and 270

--- ml-logic/ml.py
+++ ml-logic/ml.py
@@ -1,30 +1,40 @@
 from sklearn.model_selection import train_test_split
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
 
 def handleData(data):
-    df = pd.DataFrame(data)
-    
-    # x = df.drop(labels="tmin", axis=1)
-    x = df.drop(labels=["tmax", "date", "tmoy"], axis=1)
-    y = df["tmin"]
-    y = y.astype('int')
+      df = pd.DataFrame(data)
 
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+      # x = df.drop(labels="tmin", axis=1)
+      x = df.drop(labels=["tmax", "date", "tmoy", "tmin"], axis=1)
+      y = df.drop(labels=["dayInYear", "date"], axis=1)
+      y = y.astype('int')
 
-    X_train.head()
+      X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
-    from sklearn.ensemble import RandomForestRegressor
-    from sklearn.linear_model import LogisticRegression
+      X_train.head()
 
+      model = LinearRegression()
 
-    model = LogisticRegression(random_state=0, solver='lbfgs')
+      model.fit(X_train, y_train)
+      score = model.score(x, y)
+      print("Score = ", score)
+      coef = model.coef_
+      print("Coef = ", coef)
+      intercept = model.intercept_
+      print("Intercept = ", intercept)
 
-    # y_train = y_train.astype('int')
+      predict = model.predict(np.array([[166], [270]]))
 
-    model.fit(X_train, y_train)
-    
-    preds = model.predict(X_test)
-
-    model.predict_proba(X_test)
-
-    print(model.score(x, y))
+      print("-----------------------------------")
+      print("Result")
+      print("-----------------------------------")
+      # r2 = r2_score(y_train, predict)
+      print("                   TEMPMAX     / TEMPMIN  / TEMPMOY")
+      print("Predict day 166 = ", predict[0])
+      print("                   TEMPMAX     / TEMPMIN  / TEMPMOY")
+      print("Predict day 270 = ", predict[1])
